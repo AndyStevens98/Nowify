c11e9a03d25e986ce30b46e42598e998a0a68aa1
Andy Stevens <andrew.stevens@gtri.gatech.edu>
2023-03-27 16:06:40 -0400

    Update Authorise.vue
    Change scope to allow queue reading

--- src/components/Authorise.vue
+++ src/components/Authorise.vue
@@ -199,7 +199,7 @@ export default {
             .substring(5)
         ].join('-')
       )
-      searchParams.append('scope', 'user-read-currently-playing')
+      searchParams.append('scope', 'user-read-playback-state')
 
       return `${this.endpoints.auth}?${searchParams.toString()}`
     }
